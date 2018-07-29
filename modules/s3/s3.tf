3fe10ce11b7af4045a100b52c76cd0130ed4ff7c
Alan Clark <alan@thatscottishdatascientist.com>
2018-07-29 12:32:48 +0100

    Forcing the deletion of S3 bucket when calling terraform destroy; otherwise bucket will only be deleted if empty

--- modules/s3/s3.tf
+++ modules/s3/s3.tf
@@ -4,6 +4,7 @@ variable "kms_key_alias_arn" {}
 resource "aws_s3_bucket" "s3_lambda_code_bucket" {
   bucket = "${var.s3_lambda_code_bucket_name}"
   acl = "private"
+  force_destroy = true
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
